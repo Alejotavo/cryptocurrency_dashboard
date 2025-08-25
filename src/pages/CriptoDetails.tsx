cfa3a13e2137922766bc18a6bfdbc5fb7b24b2f6
Alejo <alejogustavof@gmail.com>
2025-08-25 07:32:16 -0300

    refactor details page

--- src/pages/CriptoDetails.tsx
+++ src/pages/CriptoDetails.tsx
@@ -30,16 +30,25 @@ function CryptoDetailsPage() {
   return (
   <div className="container mx-auto mt-10">
     <div className="flex flex-col md:flex-row">
-        <div className="w-full md:w-1/3 p-4">
-          <div className="border border-gray-200 p-4">
-            <label className="block font-semibold">{crypto.name}</label>
-            <p className="mb-4">{crypto.symbol.toUpperCase()}</p>
-            <p>{crypto.current_price.toLocaleString("en-US", {
-                style: "currency",
-                currency: "USD",
-                maximumFractionDigits: 2, 
-                })}</p>
-            <img src={crypto.image} alt={crypto.name} width={40} />
+        <div className="w-full md:w-1/3 p-4 border-r border-gray-200 ">
+          <div className="p-4">
+            <div className="flex flex-row items-center mb-4 gap-2">
+              <img src={crypto.image} alt={crypto.name} width={30} />
+              <label className="block font-semibold">{crypto.name}</label>
+              <label className="text-sm  text-gray-400">{crypto.symbol.toUpperCase()}</label>
+            </div>
+            <div className="flex flex-col ml-10 text-2xl">
+              <p>{crypto.current_price.toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                  maximumFractionDigits: 2, 
+                  })}{" "}USD
+              </p>
+              <div className={`mt-4 text-lg ${crypto.price_change_percentage_24h < 0 ? 'text-red-500' : 'text-green-500'}` }>
+                {crypto.price_change_percentage_24h < 0 ? "↓" : "↑"}{" "}
+                {Math.abs(crypto.price_change_percentage_24h).toFixed(1)}%
+              </div>
+            </div>
           </div>
           <div className="flex flex-col items-center">
             
@@ -50,7 +59,7 @@ function CryptoDetailsPage() {
           </div>
           <div className="w-full p-4">
               <div className="h-[300px]"> 
-                <h2>Price Variation from Last 7 Days</h2>
+                <h2 className="text-lg font-semibold p-4">Price Variation from Last 7 Days</h2>
                 <ResponsiveContainer width="100%" height="100%">
                   <LineChart data={chartData}>
                     <CartesianGrid strokeDasharray="3 3" />
